bc11b441ee97d7f4cc879012b0a39889c805de55
Maciej Szalewicz <m.szalewicz@gmail.com>
2026-01-03 16:29:03 +0100

    rustlings 03_if

--- exercises/rustlings/exercises/03_if/if1.rs
+++ exercises/rustlings/exercises/03_if/if1.rs
@@ -4,6 +4,11 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+
+    match a > b {
+        true => return a,
+        false => return b
+    };
 }
 
 fn main() {
