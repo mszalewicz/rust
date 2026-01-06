47f5fd66b7c9adcb20e59953f6a234312aed828f
Maciej Szalewicz <m.szalewicz@gmail.com>
2026-01-06 18:22:32 +0100

    rustlings 05_vecs

--- exercises/rustlings/exercises/05_vecs/vecs1.rs
+++ exercises/rustlings/exercises/05_vecs/vecs1.rs
@@ -1,5 +1,8 @@
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // Array
+    let v = vec![10, 20, 30, 40];
+
+    // let v: std::vec::Vec<i32> = (1..=4).map(|i| i * 10).collect();
 
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
