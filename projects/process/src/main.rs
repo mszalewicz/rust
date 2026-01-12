06dd3261df8f7e715b6d5bbce52d8f6322468492
Maciej Szalewicz <m.szalewicz@gmail.com>
2026-01-12 13:05:04 +0100

    rust process testing

---
+++ projects/process/src/main.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+fn main() {
+	let output = Command::new("ls")
+		.arg("-ls")
+		.output()
+		.expect("Failed to execute command");
+
+	let stdout = String::from_utf8_lossy(&output.stdout);
+
+	println!("{}", stdout);
+	println!("{}", output.status.success());
+
+}
